ee8755ccf084800c4db5a7dec13143d0505f95df
Yvgen Troshchiy <yvgen.troshchiy@gmail.com>
2021-12-21 18:01:35 +0200

    Update Dependencies import

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,3 @@
-import Dependencies.composeVersion
-
 plugins {
     id("com.android.application")
     kotlin("android")
@@ -39,7 +37,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = composeVersion
+        kotlinCompilerExtensionVersion = Dependencies.composeVersion
     }
     packagingOptions {
         resources {
@@ -55,9 +53,9 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.4.0")
     implementation("com.google.android.material:material:1.4.0")
 
-    implementation("androidx.compose.ui:ui:$composeVersion")
-    implementation("androidx.compose.material:material:$composeVersion")
-    implementation("androidx.compose.ui:ui-tooling-preview:$composeVersion")
+    implementation("androidx.compose.ui:ui:${Dependencies.composeVersion}")
+    implementation("androidx.compose.material:material:${Dependencies.composeVersion}")
+    implementation("androidx.compose.ui:ui-tooling-preview:${Dependencies.composeVersion}")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.4.0")
     implementation("androidx.activity:activity-compose:1.4.0")
 
@@ -65,7 +63,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:$composeVersion")
-    debugImplementation("androidx.compose.ui:ui-tooling:$composeVersion")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:${Dependencies.composeVersion}")
+    debugImplementation("androidx.compose.ui:ui-tooling:${Dependencies.composeVersion}")
     // endregion
 }
