6e8370b66d7065c36a4db2e0cf694380d293d0df
Yvgen Troshchiy <yvgen.troshchiy@gmail.com>
2021-12-22 00:56:04 +0200

    Back from kts

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,3 +1,5 @@
+// AndroidStudio won't highlight outdate library after switch to use Kotlin DSL
+// https://stackoverflow.com/questions/62729468/androidstudio-wont-highlight-outdate-library-after-switch-to-use-kotlin-dsl
 object Dependencies {
 
     const val composeVersion = "1.0.1"
@@ -9,7 +11,7 @@ object Dependencies {
     }
 
     object Network {
-        const val retrofit2 = "com.squareup.retrofit2:retrofit:2.9.0"
+        const val retrofit2 = "com.squareup.retrofit2:retrofit:2.8.0"
         const val retrofit2ConverterGson = "com.squareup.retrofit2:converter-gson:2.9.0"
         const val okhttp3LoggingInterceptor = "com.squareup.okhttp3:logging-interceptor:4.9.0"
         const val gson = "com.google.code.gson:gson:2.8.9"
