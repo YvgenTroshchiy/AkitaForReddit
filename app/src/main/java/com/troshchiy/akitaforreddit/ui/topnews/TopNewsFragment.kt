ebb74471ad01d3355835a2db5c3c093e26991a49
Yvgen Troshchiy <yvgen.troshchiy@gmail.com>
2022-02-02 12:07:40 +0200

    Move loadTopNews to VM

--- app/src/main/java/com/troshchiy/akitaforreddit/ui/topnews/TopNewsFragment.kt
+++ app/src/main/java/com/troshchiy/akitaforreddit/ui/topnews/TopNewsFragment.kt
@@ -2,7 +2,6 @@ package com.troshchiy.akitaforreddit.ui.topnews
 
 import android.content.Context
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -22,8 +21,6 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -31,26 +28,19 @@ import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.lifecycleScope
 import androidx.navigation.findNavController
 import coil.annotation.ExperimentalCoilApi
 import com.troshchiy.akitaforreddit.R
 import com.troshchiy.akitaforreddit.appComponent
-import com.troshchiy.akitaforreddit.extensions.toast
-import com.troshchiy.akitaforreddit.network.RedditService
 import com.troshchiy.akitaforreddit.network.data.RedditPost
-import com.troshchiy.akitaforreddit.network.data.mapToDomainModel
 import com.troshchiy.akitaforreddit.ui.theme.AkitaForRedditTheme
 import com.troshchiy.akitaforreddit.ui.topnews.components.PostCard
 import javax.inject.Inject
-import kotlinx.coroutines.launch
 
 @ExperimentalCoilApi
 class TopNewsFragment : Fragment() {
 
-    @Inject lateinit var service: RedditService
-
-    val posts: MutableState<List<RedditPost>> = mutableStateOf(ArrayList())
+    @Inject lateinit var viewModel: TopNewsViewModel
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -58,18 +48,16 @@ class TopNewsFragment : Fragment() {
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        lifecycleScope.launch {
-            loadTopNews()
-        }
-
         return ComposeView(requireContext()).apply {
             setContent() {
+                val posts = viewModel.posts.value
+
                 LazyColumn(
                     modifier = Modifier
                         .fillMaxSize()
                         .padding(top = 8.dp)
                 ) {
-                    itemsIndexed(items = posts.value) { index: Int, post: RedditPost ->
+                    itemsIndexed(items = posts) { index: Int, post: RedditPost ->
                         PostCard(post) {
 //                                openNewsDetailsFragment(view)
                             findNavController().navigate(R.id.action_topNewsFragment_to_newsDetailsFragment)
@@ -85,22 +73,7 @@ class TopNewsFragment : Fragment() {
         view.findNavController().navigate(R.id.action_topNewsFragment_to_newsDetailsFragment)
         Toast.makeText(context, "Showing toast....", Toast.LENGTH_SHORT).show()
     }
-
-    // TODO: temp. should be in the VM
-    private suspend fun loadTopNews() {
-        val result = service.topNews(10, null)
-
-        if (result.isSuccessful && result.body() != null) {
-            val right = result.body()!!.mapToDomainModel()
-            posts.value = right
-            Log.d("TopNewsActivity", "result: $right")
-        } else {
-            Log.w("TopNewsActivity", "Error by loading: ${result.message()}")
-            requireContext().toast("Error by loading")
-        }
-    }
 }
-
 
 @Composable
 fun TopNewsList() {
--- app/src/main/java/com/troshchiy/akitaforreddit/ui/topnews/TopNewsViewModel.kt
+++ app/src/main/java/com/troshchiy/akitaforreddit/ui/topnews/TopNewsViewModel.kt
@@ -1,6 +1,36 @@
 package com.troshchiy.akitaforreddit.ui.topnews
 
+import android.util.Log
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.troshchiy.akitaforreddit.network.RedditService
+import com.troshchiy.akitaforreddit.network.data.RedditPost
+import com.troshchiy.akitaforreddit.network.data.mapToDomainModel
+import javax.inject.Inject
+import kotlinx.coroutines.launch
 
-class TopNewsViewModel : ViewModel() {
+class TopNewsViewModel @Inject constructor(private val service: RedditService) : ViewModel() {
+
+    val posts: MutableState<List<RedditPost>> = mutableStateOf(ArrayList())
+
+    init {
+        loadTopNews()
+    }
+
+    private fun loadTopNews() {
+        viewModelScope.launch {
+            val result = service.topNews(10, null)
+
+            if (result.isSuccessful && result.body() != null) {
+                val right = result.body()!!.mapToDomainModel()
+                posts.value = right
+                Log.d("TopNewsActivity", "result: $right")
+            } else {
+                Log.w("TopNewsActivity", "Error by loading: ${result.message()}")
+//                requireContext().toast("Error by loading")
+            }
+        }
+    }
 }
